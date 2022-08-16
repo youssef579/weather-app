bc93f4de93bda0896c4278640ec7d5de591095c3
Youssef Ragaey <youssefragaa3@gmail.com>
2022-08-16 15:14:06 +0200

    Finish the project

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 # Imports
-from flask import Flask, render_template, request, flash, redirect, session
+from flask import Flask, render_template, request, flash, session
 import secrets, requests
-from flask_sqlalchemy import SQLAlchemy
+from flask_session import Session
 
 # Global variables
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-db = SQLAlchemy(app)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 API = '9dd5a007254678bd11aee4e1877d4cf2'
 
 # Send a http request to get the weather from api
@@ -19,51 +20,39 @@ def get_weather(name):
             return None
             
         case _:
-            city = {
+            return {
                 'name': name.title(),
                 'c': round(result['main']['temp'] - 273.15),
                 'f': round(result['main']['temp'] * 1.8 - 459.67),
                 'description': result['weather'][0]['description'],
                 'icon': result['weather'][0]['icon']
             }
-            return city
-
-# Database
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False, unique=True)
-    password = db.Column(db.String(50), nullable=False)
-    
-class City(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(40), nullable=False, unique=True)
     
 # Main route
 @app.route('/', methods=["GET", "POST"])
 def index():
-    cities = [get_weather(city.name) for city in City.query.all()]
+    if 'cities' not in session:
+        session['cities'] = []
     
     match request.method:
         case "GET":
-            return render_template('index.html', cities=cities)
+            return render_template('index.html', cities=session['cities'])
         
         case "POST":
             city_name = request.form.get('city', '').strip()
             
             if (city := get_weather(city_name)) != None:
-                for i in cities:
+                for i in session['cities']:
                     if i['name'] == city['name']:
                         flash(f"You've added \"{city['name']}\" already.", "warning")
                         break
                 else:
-                    db.session.add(City(name=city['name']))
-                    db.session.commit()
-                    cities.append(city)
+                    session['cities'].append(city)
                     
-                return render_template('index.html', cities=cities)
+                return render_template('index.html', cities=session['cities'])
             
             flash(f'"{city_name}" is not found.', "error")
-            return render_template('index.html', cities=cities)
+            return render_template('index.html', cities=session['cities'])
 
 match __name__:
     case "__main__":
